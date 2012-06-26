e9f14172e8c1b4b947d48467a36b62e472633eef
Tom <tom@smudge.com>
2012-06-27 09:01:36 +1200

    added project in

---
+++ BuildTest/src/nz/co/buildtest/smudgeapps/com/BuildTestActivity.java
@@ -0,0 +1,13 @@
+package nz.co.buildtest.smudgeapps.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BuildTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
